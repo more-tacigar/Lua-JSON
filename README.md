382658447e7144a1940107838127e325a38d9eaf
tacigar <ig.lhound@gmail.com>
2016-05-24 20:56:04 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# JSON parser written in Lua
+
+## Usage
+
+```lua
+local Json = require("json")
+local res = Json.parse("test.json")
+```
---
+++ src/json.lua
@@ -0,0 +1,17 @@
+------------------------------------------------------------
+-- Copyright (c) 2016 tacigar
+-- https://github.com/tacigar/lua-json
+------------------------------------------------------------
+
+local Parser = require("parser")
+
+json = {}
+
+function json.parse(filename)
+    local input = io.input(filename)
+    return Parser.parse({
+	    read_char = function(self) return input:read(1) end	    
+    })
+end
+
+return json
